f719abbc05f0c89c7c322846dcd44180feae03ad
XavierRao <xavierrao23@gmail.com>
2025-02-25 11:56:06 -0500

    Added getters to User class

--- src/main/java/edu/rit/swen262/user/User.java
+++ src/main/java/edu/rit/swen262/user/User.java
@@ -2,14 +2,14 @@ package edu.rit.swen262.user;
 
 import java.util.Date;
 
-// import edu.rit.swen262.service.GoalService;
+import edu.rit.swen262.service.GoalService;
 
 public class User {
     private String name;
     private double height;
     private double weight;
     private Date birthdate;
-    // private GoalService goalService;
+    private GoalService goalService;
     // private ShoppingListService shoppingListService;
     // private DailyHistory dailyHistory;
 
@@ -18,5 +18,26 @@ public class User {
         this.height = height;
         this.weight = weight;
         this.birthdate = birthdate;
+        goalService = new GoalService(false, 0, 0);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    
+    public double getHeight() {
+        return this.height;
+    }
+
+    public double getWeight() {
+        return this.weight;
+    }
+
+    public Date getBirthdate() {
+        return this.birthdate;
+    }
+
+    public GoalService getGoalService() {
+        return this.goalService;
     }
 }
