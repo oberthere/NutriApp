75d1ec72d257dbd8739d7ab97c7b902c7769ad92
hw5656 <hw5656@g.rit.edu>
2025-02-25 12:07:53 -0500

    adding Food

---
+++ src/main/java/edu/rit/swen262/food/FoodInterface.java
@@ -0,0 +1,17 @@
+package edu.rit.swen262.food;
+
+public interface FoodInterface {
+    
+    String getName();
+
+    int getCalories();
+
+    double getFat();
+
+    double getProtein();
+
+    double getCarbs();
+
+    double getFiber();
+
+}
