c0e8f03f1554932d00e21bd72ca2fb5d4604b2ef
MingFZ0 <mingfengz1678@gmail.com>
2025-02-26 19:13:31 -0500

    All shopping criteria has been finished

--- src/main/java/edu/shoppingList/strag/strategy/ShopCriteraLowIngredientStrategy.java
+++ src/main/java/edu/shoppingList/strag/strategy/ShopCriteraLowIngredientStrategy.java
@@ -1,6 +1,10 @@
 package edu.shoppingList.strag.strategy;
 
+import java.util.Map;
+
 import edu.rit.swen262.food.Food;
+import edu.rit.swen262.food.Ingredient;
+import edu.rit.swen262.food.PantryStock;
 import edu.shoppingList.ShoppingList;
 
 /**
@@ -11,8 +15,13 @@ public class ShopCriteraLowIngredientStrategy implements ShopCriteriaStrategy {
     @Override
     public ShoppingList buildShoppingList(Food food) {
         ShoppingList shoppingList = new ShoppingList();
-        return null;
-        
+        Map<Ingredient, Integer> ingredientRecord = PantryStock.getAllIngredients();
+        for (Ingredient ingredient : ingredientRecord.keySet()) {
+            if (ingredientRecord.get(ingredient) <= ShopCriteriaStrategy.lowStockQuantityValue) {
+                shoppingList.addToShoppingList(ingredient);
+            }
+        }
+        return shoppingList;
     }
     
 }
