ee9e77eff2d8847113cb79eed138f461799db0e4
MingFZ0 <mingfengz1678@gmail.com>
2025-02-26 19:20:52 -0500

    Shopping list subsystem is done

--- src/main/java/edu/rit/swen262/user/service/ShoppingListService.java
+++ src/main/java/edu/rit/swen262/user/service/ShoppingListService.java
@@ -1,4 +1,19 @@
 package edu.rit.swen262.user.service;
 
+import edu.rit.swen262.food.Food;
+import edu.shoppingList.ShoppingList;
+import edu.shoppingList.strag.strategy.ShopCriteriaStrategy;
+
 public class ShoppingListService {
+    private ShopCriteriaStrategy shopCriteria;
+
+    public void setCriteria(ShopCriteriaStrategy shopCriteria) {
+        this.shopCriteria = shopCriteria;
+    }
+
+    public ShoppingList generateShoppingList(Food food) {
+        return this.shopCriteria.buildShoppingList(food);
+        //Only the ShopCriteriaLowRecipeStrategy requires a food input which is meant to be the recipe
+        //For other strategy, the paramter input doesn't get used.
+    }
 }
