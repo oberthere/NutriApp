dc1ed9434589b8a2493013479f5bbffda6e4e0ee
Noah Callesto <nrc3290@rit.edu>
2025-02-27 20:57:58 -0500

    Fixed MealTest

--- src/test/java/edu/rit/swen262/food/MealTest.java
+++ src/test/java/edu/rit/swen262/food/MealTest.java
@@ -14,28 +14,30 @@ class MealTest {
     @BeforeEach
     void setUp() {
         // Create recipes
-        List<Food> pancakeIngredients = Arrays.asList(
+        List<Ingredient> pancakeIngredients = Arrays.asList(
             new Ingredient("Flour", 100, 5, 1.0, 3.0, 20.0),
             new Ingredient("Milk", 50, 3, 2.0, 3.5, 5.0),
             new Ingredient("Egg", 70, 3, 5.0, 6.0, 1.0)
         );
 
-        Food pancake = new Recipe("Pancakes", pancakeIngredients, "Combine the flour, eggs, milk and salt in a large bowl and whisk to combine. Whisk it until it's smooth and there are no lumps.\r\n" + //
-                        "Pour the batter into small rounds onto a hot, lightly greased skillet and flip after about 20 to 30 seconds. Let cook for an additional 15 to 20 seconds.\r\n" + //
-                        "Serve hot with maple syrup on top. Enjoy!");
+        Recipe pancake = new Recipe("Pancakes", pancakeIngredients, 
+            "Combine the flour, eggs, milk and salt in a large bowl and whisk to combine. Whisk it until it's smooth and there are no lumps.\r\n" + //
+            "Pour the batter into small rounds onto a hot, lightly greased skillet and flip after about 20 to 30 seconds. Let cook for an additional 15 to 20 seconds.\r\n" + //
+            "Serve hot with maple syrup on top. Enjoy!");
 
 
-        List<Food> omeletteIngredients = Arrays.asList(
-            new Ingredient("Egg", 70, 70, 5.0, 6.0, 1.0),
-            new Ingredient("Cheese", 80, 135, 6.0, 5.0, 1.0, 0.0)
+        List<Ingredient> omeletteIngredients = Arrays.asList(
+            new Ingredient("Egg", 70, 7.0, 5.0, 6.0, 1.0),
+            new Ingredient("Cheese", 80, 6.0, 5.0, 1.0, 0.0)
         );
 
-        Food omelette = new Recipe("Omelette", omeletteIngredients, "Beat the eggs until smooth. Pour eggs into a pan and wait until the edges crisp. " +
-                                                                        "Then, put the cheese in the center. Fold one side of the egg over the other. Enjoy!");
+        Recipe omelette = new Recipe("Omelette", omeletteIngredients, 
+            "Beat the eggs until smooth. Pour eggs into a pan and wait until the edges crisp. " +                                                            
+            "Then, put the cheese in the center. Fold one side of the egg over the other. Enjoy!");
 
 
         // Create a meal
-        List<Food> recipes = Arrays.asList(pancake, omelette);
+        List<Recipe> recipes = Arrays.asList(pancake, omelette);
         meal = new Meal("Breakfast Combo", recipes);
     }
 
@@ -46,27 +48,27 @@ class MealTest {
 
     @Test
     void testGetCalories() {
-        assertEquals(595, meal.getCalories());
+        assertEquals(370, meal.getCalories());
     }
 
     @Test
     void testGetFat() {
-        assertEquals(19.0, meal.getFat(), 0.001);
+        assertEquals(24.0, meal.getFat(), 0.001);
     }
 
     @Test
     void testGetProtein() {
-        assertEquals(23.5, meal.getProtein(), 0.001);
+        assertEquals(18.0, meal.getProtein(), 0.001);
     }
 
     @Test
     void testGetCarbs() {
-        assertEquals(28.0, meal.getCarbs(), 0.001);
+        assertEquals(19.5, meal.getCarbs(), 0.001);
     }
 
     @Test
     void testGetFiber() {
-        assertEquals(1.0, meal.getFiber(), 0.001);
+        assertEquals(27.0, meal.getFiber(), 0.001);
     }
 
     @Test
