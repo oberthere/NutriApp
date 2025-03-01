1f6cd10eecf93d6bdddf820620cff49af5e2080e
MingFZ0 <mingfengz1678@gmail.com>
2025-03-01 14:03:50 -0500

    Meal Page is done

--- src/main/java/edu/rit/swen262/ui/pages/MealPage.java
+++ src/main/java/edu/rit/swen262/ui/pages/MealPage.java
@@ -5,6 +5,7 @@ import java.util.List;
 import edu.rit.swen262.food.Meal;
 import edu.rit.swen262.ui.PageData;
 import edu.rit.swen262.ui.commands.UserCommand;
+import edu.rit.swen262.user.service.DailyHistoryService;
 
 public class MealPage extends Page {
     private PageData pageData;
@@ -17,9 +18,17 @@ public class MealPage extends Page {
 
     @Override
     public void printContent() {
+        DailyHistoryService dh = pageData.getCurrentUser().getDailyHistoryService();
+
         super.printContent();
+        System.out.println("Meals Prepared:");
+        printMeal(dh.getPreparedMeals());
         System.out.println("Meals Consumed:");
-        for (Meal meal : pageData.getCurrentUser().getDailyHistoryService().getEatenMeals()) {
+        printMeal(dh.getEatenMeals());
+    }
+
+    private void printMeal(List<Meal> meals) {
+        for (Meal meal : meals) {
             System.out.println("\t- " + meal.getName());
         }
     }
