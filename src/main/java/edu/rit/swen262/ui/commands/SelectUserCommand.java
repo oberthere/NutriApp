5231e84bbd54acacc18c2358d2a8ed3c4dbec027
hw5656 <hw5656@g.rit.edu>
2025-03-01 13:32:42 -0500

    implemented performAction()

--- src/main/java/edu/rit/swen262/ui/commands/SelectUserCommand.java
+++ src/main/java/edu/rit/swen262/ui/commands/SelectUserCommand.java
@@ -3,6 +3,8 @@ package edu.rit.swen262.ui.commands;
 import edu.rit.swen262.ui.PageData;
 import edu.rit.swen262.ui.PageRunner;
 import edu.rit.swen262.ui.pages.Page;
+import edu.rit.swen262.ui.pages.UserDashboardPage;
+import edu.rit.swen262.ui.pages.UserSetupPage;
 import edu.rit.swen262.user.User;
 
 public class SelectUserCommand extends UserCommand {
@@ -24,13 +26,22 @@ public class SelectUserCommand extends UserCommand {
         String username = commandArgs[1];
         User user = pageData.getUser(username);
 
+        pageData.setCurrentUser(user);
+
         //Send users who started their current day history to userDashboard Page
         if (user.getDailyHistoryService() != null)
         {
-            Page userDashboard = pageData.getPage("UserDashboardPage");
-            
+            Page dashboardPage = new UserDashboardPage(pageData);
+            pageRunner.setPage(dashboardPage);
 
         }
         //Send users who didn't start on their current day history to userSetup Page
+        else {
+            Page setupPage = new UserSetupPage(pageData);
+            pageRunner.setPage(setupPage);
+        }
+
+        pageRunner.runPage();
+
     }
 }
