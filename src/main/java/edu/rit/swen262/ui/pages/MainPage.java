698ab1cbe4912e080358b1d58fe855100c0d7e0b
hw5656 <hw5656@g.rit.edu>
2025-03-03 21:42:00 -0500

    Added CreateUserCommand & SelectUserCommand to MainPage

--- src/main/java/edu/rit/swen262/ui/pages/MainPage.java
+++ src/main/java/edu/rit/swen262/ui/pages/MainPage.java
@@ -1,13 +1,20 @@
 package edu.rit.swen262.ui.pages;
 
-import java.util.ArrayList;
+
 import edu.rit.swen262.ui.PageData;
+import edu.rit.swen262.ui.PageRunner;
+import edu.rit.swen262.ui.commands.CreateUserCommand;
+import edu.rit.swen262.ui.commands.SelectUserCommand;
 import edu.rit.swen262.user.User;
 
 public class MainPage extends Page {
-    public MainPage(PageData pageData) {
+    public MainPage(PageData pageData, PageRunner pageRunner) {
         super(pageData);
         this.pageName = "Main Page";
+        
+        // MainPage Commands
+        this.userCommands.add(new CreateUserCommand(pageData));
+        this.userCommands.add(new SelectUserCommand(pageData, pageRunner)); 
     }
 
     @Override
