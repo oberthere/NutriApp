6026af10cc2cb965d4245b8a0fbd1ffb99c9ac15
Noah Callesto <nrc3290@rit.edu>
2025-03-21 21:26:26 -0400

    Updated Add Recipe Command to parse multi word arguments

--- src/main/java/edu/rit/swen262/ui/commands/AddRecipeCommand.java
+++ src/main/java/edu/rit/swen262/ui/commands/AddRecipeCommand.java
@@ -12,7 +12,6 @@ public class AddRecipeCommand extends UserCommand {
     public AddRecipeCommand() {
         super.nameString = "AddRecipe";
         super.helpString = "AddRecipe [Name] [Instruction] [IngredientID] [IngredientID] ...";
-
     }
 
     @Override
@@ -22,15 +21,53 @@ public class AddRecipeCommand extends UserCommand {
             return;
         }
 
-        List<Ingredient> ls = new ArrayList<>();
+        // build the arguments with spaces inside quotes
+        List<String> parsedArgs = new ArrayList<>();
+        StringBuilder current = new StringBuilder();
+        boolean inQuotes = false;
 
-        for (int i = 3; i < commandArgs.length; i++) {
-            int id = Integer.parseInt(commandArgs[i]);
-            Ingredient ingredient = PantryStock.getIngredientByID(id);
-            ls.add(ingredient);
+        for (String arg : commandArgs) {
+            if (arg.startsWith("\"") && !inQuotes) {
+                inQuotes = true;
+                current.append(arg.substring(1)); // removes the opening quote
+            } else if (arg.endsWith("\"") && inQuotes) {
+                inQuotes = false;
+                current.append(" ").append(arg, 0, arg.length() - 1); // removes the closing quote
+                parsedArgs.add(current.toString());
+                current = new StringBuilder();
+            } else if (inQuotes) {
+                current.append(" ").append(arg);
+            } else {
+                parsedArgs.add(arg);
+            }
         }
 
-        PantryStock.addRecipe(new Recipe(commandArgs[1], ls, commandArgs[2]));
-        System.out.println("Successfully added Recipe " + commandArgs[1]);
+        if (inQuotes) {
+            System.out.println("Error: Mismatched quotes in arguments.");
+            return;
+        }
+
+        // make srue there is enough args after parsing
+        if (parsedArgs.size() < 4) {
+            System.out.println("Error: Invalid number of arguments. Usage: " + getHelp());
+            return;
+        }
+
+        // parse the ingredient IDs
+        List<Ingredient> ingredients = new ArrayList<>();
+        for (int i = 3; i < parsedArgs.size(); i++) {
+            try {
+                int id = Integer.parseInt(parsedArgs.get(i));
+                Ingredient ingredient = PantryStock.getIngredientByID(id);
+                ingredients.add(ingredient);
+            } catch (NumberFormatException e) {
+                System.out.println("Error: Invalid ingredient ID: " + parsedArgs.get(i));
+                return;
+            }
+        }
+
+        // add the recipe to PantryStock
+        PantryStock.addRecipe(new Recipe(parsedArgs.get(1), ingredients, parsedArgs.get(2)));
+        System.out.println("Successfully added Recipe \"" + parsedArgs.get(1) + "\"");
     }
 }
