cfd3a222f15be529681a57761ecceabb873a82b4
Noah Callesto <nrc3290@rit.edu>
2025-04-04 17:49:57 -0400

    Initial goal tests

---
+++ src/test/java/edu/rit/swen262/goal/LoseWeightTest.java
@@ -0,0 +1,20 @@
+package edu.rit.swen262.goal;
+
+import org.junit.jupiter.api.Test;
+
+public class LoseWeightTest {
+    @Test
+    void testCalculateTargetCalories() {
+
+    }
+
+    @Test
+    void testChangeGoal() {
+
+    }
+
+    @Test
+    void testToString() {
+
+    }
+}
