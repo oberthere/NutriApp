279dbb3410a1d53551d153da75f3181507f48528
Noah Callesto <nrc3290@rit.edu>
2025-04-05 19:48:10 -0400

    Added workout tests

---
+++ src/test/java/edu/rit/swen262/workout/LowIntensityStrategyTest.java
@@ -0,0 +1,10 @@
+package edu.rit.swen262.workout;
+
+import org.junit.jupiter.api.Test;
+
+public class LowIntensityStrategyTest {
+    @Test
+    void testCaloriesBurned() {
+
+    }
+}
---
+++ src/test/java/edu/rit/swen262/workout/WorkoutTest.java
@@ -0,0 +1,30 @@
+package edu.rit.swen262.workout;
+
+import org.junit.jupiter.api.Test;
+
+public class WorkoutTest {
+    @Test
+    void testGetCaloriesBurned() {
+
+    }
+
+    @Test
+    void testGetDurationMin() {
+
+    }
+
+    @Test
+    void testGetIntensity() {
+
+    }
+
+    @Test
+    void testGetName() {
+
+    }
+
+    @Test
+    void testGetRecordedDate() {
+
+    }
+}
